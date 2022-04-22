357c0791744ef1d245d581cd84c6644c4378b60d
Vincenzo <ilcontevince@gmail.com>
2022-04-22 22:32:50 +0100

    Create Other Components

---
+++ client/src/components/ScheduleCards.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ScheduleCards = () => {
+  return <div>ScheduleCards</div>;
+};
+
+export default ScheduleCards;
---
+++ client/src/components/ScheduleLog.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ScheduleLog = () => {
+  return <div>ScheduleLog</div>;
+};
+
+export default ScheduleLog;
