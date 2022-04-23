5f77efeeef6e28dc97a2e35465ddbbf5d60c45e6
Vincenzo <ilcontevince@gmail.com>
2022-04-23 18:37:53 +0100

    Add Schedule Cards on the side

--- client/src/components/ScheduleCard.jsx
+++ client/src/components/ScheduleCard.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import clearTitle from '../helpers/clearTitle';
+import { AppContext } from '../store/AppContext';
+import { SELECT_CARD } from '../store/AppReducers';
 
-const ScheduleCard = () => {
-  return <div>ScheduleCard</div>;
+const ScheduleCard = ({ card, hasMargin }) => {
+  const { dispatchSelected } = useContext(AppContext);
+
+  const handleSelect = useCallback(cardId => {
+    dispatchSelected({ type: SELECT_CARD, payload: cardId });
+  }, []);
+
+  return (
+    <div
+      className={`border-2 border-black rounded-xl p-2 cursor-pointer ${
+        hasMargin ? 'my-4' : ''
+      }`}
+      onClick={() => handleSelect(card.id)}
+    >
+      <h2>{clearTitle(card.name)}</h2>
+      <p>{card.description}</p>
+      <div className="flex justify-end">
+        <button className="px-2 border-2 border-black rounded-md">
+          Retire
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ScheduleCard;
--- client/src/components/ScheduleCards.jsx
+++ client/src/components/ScheduleCards.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import ScheduleCard from './ScheduleCard';
 
 const ScheduleCards = () => {
-  return <div>ScheduleCards</div>;
+  // React Query
+  const { isLoading, data } = useQuery('schedules', async () => {
+    const { data } = await axios('http://localhost:3000/schedules');
+    return data;
+  });
+  console.log(data);
+  return (
+    <aside className="w-full max-w-xs pr-4">
+      {isLoading ? (
+        <span>Loading</span>
+      ) : (
+        data.map((card, index) => {
+          return (
+            <ScheduleCard
+              key={card.id}
+              card={card}
+              hasMargin={index && index < data.length - 1}
+            />
+          );
+        })
+      )}
+    </aside>
+  );
 };
 
 export default ScheduleCards;
